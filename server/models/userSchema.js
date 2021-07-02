3d7c7fc44f3993108db2741ec01a605104834589
himanshi0812 <himanshineema31@gmail.com>
2021-07-02 12:51:25 +0530

    add password hashed functionality

--- server/models/userSchema.js
+++ server/models/userSchema.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     name : {
@@ -11,7 +12,10 @@ const userSchema = new mongoose.Schema({
     },
     password : {
         type:String,
-        required:true
+        required:true,
+        trim: true,
+        minlength:8,
+        
     },
     cpassword : {
         type:String,
@@ -22,6 +26,17 @@ const userSchema = new mongoose.Schema({
         required:true
     }
 })
+
+
+
+userSchema.pre('save', async function(next){
+    if(this.isModified('password')){
+        this.password = await bcrypt.hash(this.password,12);
+        this.cpassword = await bcrypt.hash(this.cpassword,12);
+    }
+    next();
+});
+
 
 const User = mongoose.model('USERS',userSchema);
 
--- server/router/auth.js
+++ server/router/auth.js
@@ -7,7 +7,7 @@ router.get('/',(req,res) => {
     res.send('auth.js')
 });
 
-router.post('/signin',async (req,res) => {
+router.post('/signup',async (req,res) => {
     // console.log(req.body);
     // res.json({message:req.body});
     const { name, email, password, cpassword, address} = req.body;
@@ -22,18 +22,46 @@ router.post('/signin',async (req,res) => {
 
         if(userExist){
             return res.status(422).json({error:"email already exist"});
-        }
+        }else if (password!= cpassword){
+            return res.json ({error : "password not matched"})
+        }else {
 
         const user = new User({  name, email, password, cpassword, address });
-
-        const userRegister = await user.save();
-
-        if(userRegister){
-            res.status(201).json({ message : "register successfully"});
+        
+        await user.save();
+        res.status(201).json({ message : "register successfully"});
         } 
 
     } catch (err) {
         console.log(err);
     }
 });   
+
+// login route
+router.post('/signin',async (req,res) => {
+    // console.log(req.body);
+    // res.json({message:"aawwwww"});
+    try{
+        const {email, password } = req.body;
+        
+        if(!email || !password)
+        {
+            return res.json({error:"please fill the field"})
+        }
+
+        const userLogin = await User.findOne({email:email});
+        console.log(userLogin);
+
+        if(!userLogin){
+            res.json({erro:"user error"})
+        }
+        else{
+            res.json({message:"login success"})
+        }
+}
+    catch (err){
+        console.log(err);
+
+    }
+});
 module.exports = router;
